6c9ca15c79a54b8e22feac48d487c7b778ba0e0f
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-06-07 01:42:14 +0200

    Cleanup spec config

--- spec/controllers/redis_web_manager/configuration_controller_spec.rb
+++ spec/controllers/redis_web_manager/configuration_controller_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe RedisWebManager::ConfigurationController, type: :controller do
   routes { RedisWebManager::Engine.routes }
