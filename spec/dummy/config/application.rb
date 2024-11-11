b54b08c8d3e93afb1354ecb529249d3d73f92538
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-11-12 00:47:14 +0100

    Load default Rails settings

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -13,7 +13,7 @@ require 'redis_web_manager'
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults Rails::VERSION::STRING.to_f
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
