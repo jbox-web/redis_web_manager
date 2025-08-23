d014c9f49e12500b42a9ec54d9ef81beb8e4ce62
Nicolas Rodriguez <nico@nicoladmin.fr>
2025-08-24 00:49:43 +0200

    Fix tests

--- spec/redis_web_manager/info_spec.rb
+++ spec/redis_web_manager/info_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe RedisWebManager::Info do
     end
 
     it 'returns a memory usage value (memory_usage)' do
-      expect(info.memory_usage('test')).to be_between(52, 62)
+      expect(info.memory_usage('test')).to be_between(40, 62)
     end
 
     it 'returns a test value (get)' do
